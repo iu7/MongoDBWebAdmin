d0a5793580d7746032d39906d55c2421a4c9e04c
Alexey Abakumkin <Bobrokorm@gmail.com>
2014-04-07 13:19:22 +0400

    updated file name in python sctipt

--- server.py
+++ server.py
@@ -4,22 +4,25 @@
 from pymongo import *
 from flask import *
 
+
 flask_server = Flask(__name__)
 mongo_client = MongoClient('localhost', 27017)
 
+
 @flask_server.route('/')
 def root():
-    return render_template('root.html',
-                           database_names = mongo_client.database_names(),
-                           submit_target = url_for('query_result'))
+    return render_template('index.html',
+                           database_names=mongo_client.database_names(),
+                           submit_target=url_for('query_result'))
 
-@flask_server.route('/query_result', methods = ['POST'])
+
+@flask_server.route('/query_result', methods=['POST'])
 def query_result():
     database_name = request.form['database_name']
     query_text = request.form['query_text']
     result = mongo_client[database_name].eval(query_text)
-    return render_template('query_result.html', result_text = result)
+    return render_template('query_result.html', result_text=result)
+
 
 if __name__ == '__main__':
-    flask_server.run(debug = True)
-
+    flask_server.run(debug=True)
