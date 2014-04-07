2a8cf05325915c940c9e9cdf1dbb3987cca7becb
Alexey Abakumkin <Bobrokorm@gmail.com>
2014-04-07 19:53:19 +0400

    now treeview display collections

--- server.py
+++ server.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pymongo import *
-from flask import *
+import pymongo
+import flask
 
 
-flask_server = Flask(__name__)
-mongo_client = MongoClient('localhost', 27017)
+flask_server = flask.Flask(__name__)
+flask_server.jinja_env.trim_blocks = True
+mongo_client = pymongo.MongoClient('localhost', 27017)
 
 
 @flask_server.route('/')
 def root():
-    return render_template('index.html',
-                           database_names=mongo_client.database_names(),
-                           submit_target=url_for('query_result'))
+    c = {x: mongo_client[x].collection_names(False)
+         for x in mongo_client.database_names()}
+    return flask.render_template('index.html',
+                                 database_names=mongo_client.database_names(),
+                                 collections=c,
+                                 submit_target=flask.url_for('query_result'))
 
 
 @flask_server.route('/query_result', methods=['POST'])
 def query_result():
-    database_name = request.form['database_name']
-    query_text = request.form['query_text']
+    database_name = flask.request.form['database_name']
+    query_text = flask.request.form['query_text']
     result = mongo_client[database_name].eval(query_text)
-    return render_template('query_result.html', result_text=result)
+    return flask.render_template('query_result.html', result_text=result)
 
 
 if __name__ == '__main__':
