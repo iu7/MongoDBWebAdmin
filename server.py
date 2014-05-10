da928d4374553e5043c2fd1c4cbfa94f61643cb1
Yuri Kilochek <yuri-kilochek@desumachine.(none)>
2014-05-10 21:27:39 +0400

    Some refactorig: extracted templates to separate files and went back to server-side templating.

--- server.py
+++ server.py
@@ -10,41 +10,35 @@ flask_server = flask.Flask(__name__)
 flask_server.jinja_env.trim_blocks = True
 mongo_client = pymongo.MongoClient('localhost', 27017)
 
-@flask_server.route('/get_object')
-def get_object():
-    database_name = flask.request.args['database_name']
-    collection_name = flask.request.args['collection_name']
-    document_id = bson.ObjectId(flask.request.args['document_id'])
+@flask_server.route('/load/<database_name>/<collection_name>/<document_id>')
+def load_document(database_name, collection_name, document_id):
     database = mongo_client[database_name]
     collection = database[collection_name]
-    object = collection.find({u'_id':document_id})[0]
+    object = collection.find({u'_id':bson.ObjectId(document_id)})[0]
     del object[u'_id']
-    return json.dumps(object)
+    return flask.render_template('document.html', object=object) 
 
-@flask_server.route('/get_document_ids')
-def get_document_ids():
-    database_name = flask.request.args['database_name']
-    collection_name = flask.request.args['collection_name']
+@flask_server.route('/load/<database_name>/<collection_name>')
+def load_collection(database_name, collection_name):
     database = mongo_client[database_name]
     collection = database[collection_name]
     document_ids = [str(document[u'_id']) for document in collection.find({}, {'_id':1})]
-    return json.dumps(document_ids)
+    return flask.render_template('collection.html', document_ids=document_ids)
 
-@flask_server.route('/get_collection_names')
-def get_collection_names():
-    database_name = flask.request.args['database_name']
+@flask_server.route('/load/<database_name>')
+def load_database(database_name):
     database = mongo_client[database_name]
     collection_names = database.collection_names()
-    return json.dumps(collection_names)
+    return flask.render_template('database.html', collection_names=collection_names)
 
-@flask_server.route('/get_database_names')
-def get_database_names():
+@flask_server.route('/load')
+def load_root():
     database_names = mongo_client.database_names()
-    return json.dumps(database_names)
+    return flask.render_template('root.html', database_names=database_names)
 
 @flask_server.route('/')
 def index():
-    return flask_server.send_static_file('index.html')
+    return flask.render_template('index.html')
 
 if __name__ == '__main__':
     flask_server.run(debug=True)
--- static/scripts.js
+++ static/scripts.js
@@ -1,98 +1,69 @@
 $(document).ready(function() {
-    var objectTemplate = nunjucks.compile($('#object_template').html());
-    var documentsTemplate = nunjucks.compile($('#documents_template').html());
-    var collectionsTemplate = nunjucks.compile($('#collections_template').html());
-    var databasesTemplate = nunjucks.compile($('#databases_template').html());
-    
-    var loadObject = function(database_name, collection_name, document) {
-        var status = document.attr('status');
+    var toggleRollable = function(rollable) {
+        var status = rollable.attr('status');
         if (status == 'unrolled') {
-            document.attr('status', 'rolledup');
+            rollable.attr('status', 'rolledup');
         } else if (status == 'rolledup') {
-            document.attr('status', 'unrolled');
-        } else if (status == 'unloaded') {
-            var document_id = document.children('.id').html();
-            document.attr('status', 'loading');
-            $.getJSON('/get_object', {database_name: database_name,
-                                      collection_name : collection_name,
-                                      document_id : document_id}, function(object) {
-                var isa = function(object, type) {
-                    return type == 'null' && object == null ||
-                           type == 'boolean' && typeof object == 'boolean' ||
-                           type == 'number' && typeof object == 'number' ||
-                           type == 'string' && typeof object == 'string' ||
-                           type == 'array' && object instanceof Array ||
-                           type == 'object';
-                };
-                function render(object) {
-                    return objectTemplate.render({object: object, isa : isa, recurse : render});
-                }
-                document.append(render(object));
-                document.attr('status', 'unrolled');
-                document.find('.rollable > .header').click(function() {
-                    var object = $(this).parent();
-                    var status = object.attr('status');
-                    if (status == 'unrolled') {
-                        object.attr('status', 'rolledup');
-                    } else if (status == 'rolledup') {
-                        object.attr('status', 'unrolled');
-                    }
-                });
-            });
-        }
-    };
-    
-    var loadDocuments = function(database_name, collection) {
-        var status = collection.attr('status');
-        if (status == 'unrolled') {
-            collection.attr('status', 'rolledup');
-        } else if (status == 'rolledup') {
-            collection.attr('status', 'unrolled');
-        } else if (status == 'unloaded') {
-            var collection_name = collection.children('.name').html();
-            collection.attr('status', 'loading');
-            $.getJSON('/get_document_ids', {database_name: database_name,
-                                            collection_name : collection_name}, function(document_ids) {
-                collection.append(documentsTemplate.render({ids: document_ids}));
-                collection.attr('status', 'unrolled');
-                collection.find('.document > .id').click(function() {
-                    var document = $(this).parent();
-                    loadObject(database_name, collection_name, document);
-                });
-            });
+            rollable.attr('status', 'unrolled');
         }
     }
     
-    var loadCollections = function(database) {
-        var status = database.attr('status');
-        if (status == 'unrolled') {
-            database.attr('status', 'rolledup');
-        } else if (status == 'rolledup') {
-            database.attr('status', 'unrolled');
-        } else if (status == 'unloaded') {
-            var database_name = database.children('.name').html();
-            database.attr('status', 'loading'); 
-            $.getJSON('/get_collection_names', {database_name: database_name}, function(collection_names) {
-                database.append(collectionsTemplate.render({names: collection_names}));
-                database.attr('status', 'unrolled');
-                database.find('.collection > .name').click(function() {
-                    var collection = $(this).parent();
-                    loadDocuments(database_name, collection);
-                });
+    var loadDocument = function(database_name, collection_name, document) {
+        var document_id = document.children('.id').html();
+        document.attr('status', 'loading');
+        $.get('/load/' + database_name + '/' + collection_name + '/' + document_id, function(object) {
+            document.removeClass('rollable');
+            document.children(':not(.id):not(.content-placeholder)').remove();
+            $(object).replaceAll(document.children('.content-placeholder'));
+            document.find('.toggler').on('click', function() {
+                var object_or_array = $(this).parent();
+                toggleRollable(object_or_array);
             });
-        }
+            document.attr('status', 'unrolled');
+        });
     };
     
-    var loadDatabases = function() {
-        $.getJSON('/get_database_names', {}, function(database_names) {
-            $('#content').append(databasesTemplate.render({names: database_names}));
-            $('.database > .name').click(function() {
+    var loadCollection = function(database_name, collection) {
+        var collection_name = collection.children('.name').html();
+        collection.attr('status', 'loading');
+        $.get('/load/' + database_name + '/' + collection_name, function(documents) {
+            $(documents).replaceAll(collection.children('.content-placeholder'))
+            .children('.toggler').on('click', function() {
+                var document = $(this).parent();
+                loadDocument(database_name, collection_name, document);
+            });
+            collection.children('.toggler').off('click').on('click', function() {
+                toggleRollable(collection);
+            });
+            collection.attr('status', 'unrolled');
+        });
+    }
+
+    var loadDatabase = function(database) {
+        var database_name = database.children('.name').html();
+        database.attr('status', 'loading');
+        $.get('/load/' + database_name, function(collections) {
+            $(collections).replaceAll(database.children('.content-placeholder'))
+            .children('.toggler').on('click', function() {
+                var collection = $(this).parent();
+                loadCollection(database_name, collection);
+            });
+            database.children('.toggler').off('click').on('click', function() {
+                toggleRollable(database);
+            });
+            database.attr('status', 'unrolled');
+        });
+    };
+    
+    var loadRoot = function() {
+        $.get('/load', function(databases) {
+            $(databases).replaceAll($('#root').children('.content-placeholder'))
+            .children('.toggler').on('click', function() {
                 var database = $(this).parent();
-                loadCollections(database);
+                loadDatabase(database);
             });
         });
     };
     
-    
-    loadDatabases();
+    loadRoot();
 });
